40e911ff7c01801eed9d61b53d3c5107ea9ec957
ssnow22-star <74156772+ssnow22-star@users.noreply.github.com>
2021-10-23 12:30:53 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,22 +7,22 @@ echo "--------------------------------------------------------------------Instal
 opkg update && opkg install luci-app-clash.ipk
 
 echo echo "--------------------------------------------------------------------Download Clash Core--------------------------------------------------------------------"
-wget -O /etc/clash/clash-linux-mips-softfloat.gz https://github.com/frainzy1477/clash_dev/releases/download/v1.1.0/clash-linux-mips-softfloat.gz
+wget -O /etc/clash/clash-linux-mipsle-softfloat-v1.7.1.gz https://github.com/Dreamacro/clash/releases/download/v1.7.1/clash-linux-mipsle-softfloat-v1.7.1.gz
 
 echo echo "--------------------------------------------------------------------Update & Install Gzip--------------------------------------------------------------------"
 opkg update && opkg install gzip 
 cd /etc/clash/
 echo "--------------------------------------------------------------------Unzip Clash Core-------------------------------------------------------------------"
-gunzip clash-linux-mips-softfloat.gz
-chmod +x clash-linux-mips-softfloat
-mv clash-linux-mips-softfloat /etc/clash/clash
+gunzip clash-linux-mipsle-softfloat-v1.7.1.gz
+chmod +x clash-linux-mipsle-softfloat-v1.7.1
+mv clash-linux-mipsle-softfloat-v1.7.1 /etc/clash/clash
 
 echo "--------------------------------------------------------------------Download Classtun--------------------------------------------------------------------"
-wget -O /etc/clash/clashtun/clash-linux-mips-softfloat-20210310.gz https://github.com/comzyh/clash/releases/download/20210310/clash-linux-mips-softfloat-20210310.gz
+wget -O /etc/clash/clashtun/clash-linux-mipsle-softfloat-20210310.gz https://github.com/comzyh/clash/releases/download/20210310/clash-linux-mipsle-softfloat-20210310.gz
 cd /etc/clash/clashtun/
-gunzip clash-linux-mips-softfloat-20210310.gz
-chmod +x clash-linux-mips-softfloat-20210310
-mv clash-linux-mips-softfloat-20210310 /etc/clash/clashtun/clash
+gunzip clash-linux-mipsle-softfloat-20210310.gz
+chmod +x clash-linux-mipsle-softfloat-20210310
+mv clash-linux-mipsle-softfloat-20210310 /etc/clash/clashtun/clash
 
 echo "--------------------------------------------------------------------Download dtun (premium core)--------------------------------------------------------------------"
 wget -O /etc/clash/dtun/clash-linux-mipsle-softfloat-2021.09.15.gz https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-mipsle-softfloat-2021.09.15.gz
